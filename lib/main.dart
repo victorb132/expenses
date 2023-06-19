18fbcdd3abebf1dd7788146f52af46b137c012fd
Victor Novais <victor96novais@gmail.com>
2023-06-19 12:40:05 -0300

    feature/initialize-app

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(const ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: const Column(
+        children: [
+          Card(
+            child: Text('Gráfico'),
+            elevation: 5,
+          ),
+          Card(
+            child: Text('Lista de transações'),
+          ),
+        ],
+      ),
+    );
+  }
+}
