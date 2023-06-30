e875bb4852cbe9631ff16a3128e03e749d659ef2
Victor Novais <victor96novais@gmail.com>
2023-06-30 17:05:58 -0300

    fix/fitted-text

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -56,9 +56,7 @@ class ChartBar extends StatelessWidget {
             SizedBox(height: constraints.maxHeight * 0.05),
             SizedBox(
               height: constraints.maxHeight * 0.15,
-              child: FittedBox(
-                child: Text(label),
-              ),
+              child: Text(label),
             ),
           ],
         );
